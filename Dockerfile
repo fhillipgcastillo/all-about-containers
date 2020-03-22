eb1902286f6eed20f50b38c115be426b6cf95462
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2020-03-22 00:27:12 -0400

    step one just basic of chroot done and documented

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
 FROM ubuntu:bionic
 RUN apt-get update
-RUN apt-get install debootstrap -y
-WORKDIR /home/chr00t
-COPY . .
 CMD [ "/bin/bash" ]
--- chroot/steps.sh
+++ chroot/steps.sh
@@ -1,12 +1,19 @@
 # step 1
+# Needs to create a directoey to apply the new root
+# option 1: for use of Dockerfile and auto scripts
+dirname=$1
+mkdir $dirname
+
+# option 2: for manual execution
 echo Directory Name  && read dirname && mkdir $dirname
 # echo echo Directory Name > steps.sh \&\& echo read dirname >> steps.sh \&\& echo mkdir \$dirname >> steps.sh \&\& echo chmod \+x steps.sh
 
-# step 2 bin and bash for xhroot
+# step 2 
+#  to copy qll executable bins files and dira with the required libs to the new directory
 cd $dirname && ldd /bin/bash && mkdir bin && mkdir lib{,64} && cp -r /bin/* bin/ && cp -r /lib/* lib/ && cp -r /lib64/* lib64/
 # echo cd \$dirname \&\& ldd /bin/bash \&\& mkdir bin \&\& mkdir lib{,64} \&\& cp -r /bin/* bin/ \&\& cp -r /lib/* lib/ >> steps.sh \&\& echo cp -r /lib64/* /lib64/ >> steps.sh
 
 # step 3 chroot
-echo secret info > secret.txt
-chroot . bash
+# I created a secret file just to hqve something and execute the chroot <dir-path> <command> to change the root directory and execute bash i side it
+echo secret info > secret.txt && chroot . bash
 # echo secret info > secret.txt && chroot . bash
